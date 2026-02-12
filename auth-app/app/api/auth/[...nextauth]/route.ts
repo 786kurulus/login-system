5aef1399ee2299c21a04c5bf543934b3f1ab7338
786kurulus <lifestealserver462@gmail.com>
2026-02-12 21:08:44 +0530

    Integrate NextAuth for authentication handling

--- auth-app/app/api/auth/[...nextauth]/route.ts
+++ auth-app/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,13 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs"; // better for Next.js environments
 import User from "../../../../models/User";
 import { connectDB } from "../../../../lib/db";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
 
 
 // ✅ Extend Session + User types safely
